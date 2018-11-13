0b71b40a05cee936c4ce28ff76d58ce808dc6b36
Oleksii Lebedev <lebediew.oleksii@gmail.com>
2018-11-13 15:45:19 +0200

    fixed colors

--- dom1.js
+++ dom1.js
@@ -1,28 +1,32 @@
 const WrapperDiv = document.querySelector('.Wrapper');
 
 function generateColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16)
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  return "rgb(" + x + "," + y + "," + z + ")"
 }
 
-document.body.onload = getOneBlock;
-  function getOneBlock(backgroundColor) {
-    var div = document.createElement('div');
-    div.style.display = 'flex';
-    div.style.justifyContent = 'center';
-    div.style.alignItems = 'center';
-    div.style.width = '50px';
-    div.style.height = '50px';
-    div.style.borderRadius = '5px';
-    // div.style.backgroundColor = color;
-    div.innerHTML = [i];
-    div.style.font = '13px Tahoma';
-    WrapperDiv.appendChild(div);
-  } 
+function getOneBlock(backgroundColor, i) {
+  var div = document.createElement('div');
+  div.style.display = 'flex';
+  div.style.justifyContent = 'center';
+  div.style.alignItems = 'center';
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.borderRadius = '5px';
+  div.style.backgroundColor = backgroundColor;
+  div.style.border = '1.2px solid rgb(0, 0, 0)';
+  div.style.boxShadow = '10px 10px 8px -7px rgba(0,0,0,0.75), 5px 5px 19px -5px #ffffff inset'
+  div.innerHTML = [i];
+  div.style.font = '13px Tahoma';
+  WrapperDiv.appendChild(div);
+} 
 
 function squareCreate(number){
+  for(i=0;i<number; i++){
     var color = generateColor()
-    for(i=1;i<number; i++){
-        getOneBlock(color);
-    }
+    getOneBlock(color, i);
   }
+}
 squareCreate(25);
